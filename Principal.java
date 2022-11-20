7d3e14c51384afd7d75a028be6ffb74bde09b195
AlexRF14 <115222489+AlexRF14@users.noreply.github.com>
2022-11-20 19:57:20 +0100

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Principal{
+	public static void main(String args []){
+	         int x , y;
+		Scanner sc = new Scanner(System.in);
+
+
+		System.out.println("Escriba un primer número entero");
+		x = sc.nextInt();
+		System.out.println("Escriba un segundo número entero");
+                y = sc.nextInt();
+	        System.out.println("El resultado de la suma de esos números es " + (x + y));
+	}
+}
+
+                          
+
